ca7f96efda4b5bdb68d0efd4ee6336f65ab45843
Griffin Wheeler <Griffin.Wheeler@firstcitizens.com>
2023-12-08 14:51:11 -0500

    Implemented evolve and material theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EvHeaderComponent } from "@evolve/angular-components";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EvHeaderComponent,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
